cfbb6313ab274e8e8516883376fe14fd3b6b3acf
hawaiiH <vladgusakov@gmail.com>
2020-10-28 18:22:06 +0200

    Updates

---
+++ precache-manifest.44d08438f08d95f71dbe21d161d5549e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c52870baecbf9f42435",
+    "url": "/udemy_react_GOT_build/static/css/main.ab4701dd.chunk.css"
+  },
+  {
+    "revision": "4c52870baecbf9f42435",
+    "url": "/udemy_react_GOT_build/static/js/main.4c52870b.chunk.js"
+  },
+  {
+    "revision": "9bb4e1250767916a093d",
+    "url": "/udemy_react_GOT_build/static/css/1.7d4f03b8.chunk.css"
+  },
+  {
+    "revision": "9bb4e1250767916a093d",
+    "url": "/udemy_react_GOT_build/static/js/1.9bb4e125.chunk.js"
+  },
+  {
+    "revision": "b5a5aab0e145d5dfc207",
+    "url": "/udemy_react_GOT_build/static/js/runtime~main.b5a5aab0.js"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/udemy_react_GOT_build/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/udemy_react_GOT_build/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "f9064bd6a046073c57a7d72328fe6da8",
+    "url": "/udemy_react_GOT_build/index.html"
+  }
+];
